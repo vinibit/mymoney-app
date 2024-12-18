2088daea8dd8b9c3ee8e83037dddee30d34fa7b9
Vinicius Martins <vinibitmartins@gmail.com>
2024-12-18 20:30:21 -0300

    Integrate Redux for state management and set up routing in the application

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./main/App"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import promise from 'redux-promise'
 
-ReactDOM.render(<App />, document.getElementById("app"))
+import Reducers from './main/Reducers'
+import Routes from './Main/Routes'
+
+const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(Reducers, DevTools)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+, document.getElementById('app'))
