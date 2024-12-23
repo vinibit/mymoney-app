7891b9a678869f9be851d1c9ee70dc08b7be28bb
Vinicius Martins <vinibitmartins@gmail.com>
2024-12-23 16:34:49 -0300

    Fix import path for Reducers to use correct casing in index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
 
-import Reducers from './main/Reducers'
+import Reducers from './Main/Reducers'
 import Routes from './Main/Routes'
 
 const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
